7ce2e7e9fd1a3d3a3deb573e69e4d02a0636d868
Dang Tuan Anh <md.totty@gmail.com>
2018-01-20 19:26:47 +0700

    test

--- src/ObjectInterface.php
+++ src/ObjectInterface.php
@@ -3,4 +3,12 @@
 namespace Stf\Object;
 
 interface ObjectInterface
-{}
+{
+	public function getName();
+
+	public function setValue($value);
+
+	public function hasValue();
+
+	public function getValue();
+}
